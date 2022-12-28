a0f1aea434368cfbd69818ed0b66466cb83ce558
Serj Tubin <stubin87@gmail.com>
2022-12-28 20:50:26 +0000

    removed placeholder index; components/layout added

---
+++ components/layout.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import styles from './layout.module.css'
+import Link from 'next/link'
+
+export const siteTitle = '2beens v2'
+
+export default function Layout({
+  children,
+  home
+}: {
+  children: React.ReactNode
+  home?: boolean
+}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  )
+}
