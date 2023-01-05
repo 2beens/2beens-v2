92357b685f30c51bada29c616edfb94d05ab1d14
Serj Tubin <stubin87@gmail.com>
2023-01-05 23:12:50 +0100

    dont fetch api from getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,29 +27,11 @@ export default function Home(props: { examplePost: String }) {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const examplePostEndpoint = '/api/post/100';
-
-  let examplePost: String;
-  try {
-    const response = await fetch(examplePostEndpoint);
-    if (response.status >= 300) {
-      examplePost = 'error' + response.status;
-    } else {
-      examplePost = await response.json();
-    }
-  } catch (e) {
-    if (typeof e === 'string') {
-      examplePost = e.toUpperCase();
-    } else if (e instanceof Error) {
-      examplePost = e.message;
-    } else {
-      examplePost = 'unkown error';
-    }
-  }
+  // TODO: fetch posts
 
   return {
     props: {
-      examplePost: examplePost,
+      examplePost: 'TODO: blog posts will appear here',
     },
   };
 };
